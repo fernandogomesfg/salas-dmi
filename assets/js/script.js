d073adaa34abcd1e16ee69270fc9d2340ad5dea4
Fernando Gomes <fernandogomesbernardo@gmail.com>
2023-03-04 22:40:31 -0800

    chore: criacao do mapa e adicao do baseMaps

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var map = L.map('map').setView([-25.94972974, 32.59967431], 16);
+
+
+var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+})
+
+var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+})
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19
+}).addTo(map);
+
+var baseMaps = {
+    'Google Satelite': googleSat,
+    'Google Streets': googleStreets,
+    'OSM': osm
+}
+
+L.control.layers(baseMaps).addTo(map);
+
+
+
+
+
